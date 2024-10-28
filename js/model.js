06698348e0f78d10b31fe17ea44f349beacd04e5
Borman <104533223+hig4vor79@users.noreply.github.com>
2024-10-28 13:02:41 +0200

    ааа

--- js/model.js
+++ js/model.js
@@ -31,25 +31,58 @@ window.addEventListener("resize", updateRendererSize);
 // Инициализируем размеры рендера
 updateRendererSize();
 
+const group = new THREE.Group();
+
 // Создаем куб
-const geometryModel = new THREE.BoxGeometry(3, 3, 3, 10, 10, 10);
-const materialModel = new THREE.MeshBasicMaterial({
+const geometryModelCube = new THREE.BoxGeometry(2, 2, 2, 1, 1, 1);
+const geometryModelCircle = new THREE.SphereGeometry(2);
+
+const materialModelWhite = new THREE.MeshBasicMaterial({
   color: 0xffffff,
   wireframe: true,
 });
-const cube = new THREE.Mesh(geometryModel, materialModel);
-sceneModel.add(cube);
+const materialModelRed = new THREE.MeshBasicMaterial({
+  color: 0xff0000,
+  wireframe: true,
+});
+
+const cube = new THREE.Mesh(geometryModelCube, materialModelWhite);
+const sircle = new THREE.Mesh(geometryModelCircle, materialModelRed);
+
+// sircle.rotation.set(Math.PI / 4, Math.PI / 4, Math.PI / 4);
+
+group.add(cube);
+group.add(sircle);
+
+sceneModel.add(group);
 
 // Устанавливаем позицию камеры
 cameraModel.position.z = 5;
 
-// Анимация
+rendererModel.render(sceneModel, cameraModel);
+
+const clock = new THREE.Clock();
+let previousTime = 0;
+
+// Анимация;
 function animateModel() {
   requestAnimationFrame(animateModel);
 
   // Задаем вращение куба
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
+  group.children[0].rotation.x += 0.01;
+  group.children[0].rotation.y += 0.01;
+
+  group.children[1].rotation.x -= 0.012;
+  group.children[1].rotation.y -= 0.012;
+
+  let currentTime = clock.getElapsedTime();
+  let delta = currentTime - previousTime;
+  previousTime = currentTime;
+  let sinDelta = Math.sin(currentTime);
+
+  group.children[1].scale.y = sinDelta * 1;
+  group.children[1].scale.z = sinDelta * 1;
+  group.children[1].scale.x = sinDelta * 1;
 
   // Рендер сцены
   rendererModel.render(sceneModel, cameraModel);
